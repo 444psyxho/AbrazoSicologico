a942b396edb321d4ae5411d2b7f234d80576fada
444psyxho <136862034+444psyxho@users.noreply.github.com>
2023-08-07 17:44:05 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function cambiaMensaje() {
 setTimeout(function (){
     alert("Recibiste un abrazo sicologico de su humilde servidor te amo demasiado muak!")
 }, 2400)
+
+setTimeout(function (){
+    alert("no mentira yo no creo en esto del amor xupala wuajaja")
+}, 2500)
+
+setTimeout(function (){
+    alert("xupala xupala xupala wuajaja")
+}, 2600)
+
+setTimeout(function (){
+    alert("👻👻👻👻👻👻👻")
+}, 2700)
